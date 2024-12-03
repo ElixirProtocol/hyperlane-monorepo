2bcf8d60c6fd5042959eec2f4196b479c4160deb
Shaun Khundker <shaun.khundker@gmail.com>
2024-12-03 16:46:49 -0500

    fmt

--- solidity/script/upgrade/UpgradeDeUSDtoFastUSDRouteMainnet.s.sol
+++ solidity/script/upgrade/UpgradeDeUSDtoFastUSDRouteMainnet.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 
 import {ITransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {HypERC20Collateral} from "contracts/token/HypERC20Collateral.sol";
 
 contract DeployBase is Script {
     function setup() internal {
@@ -14,15 +15,33 @@ contract DeployBase is Script {
         // Read caller information.
         (, address deployer, ) = vm.readCallers();
 
+        // Mainnet addresses
+        address proxyAddress = 0x9AD81058c6C3Bf552C9014CB30E824717A0ee21b;
+        address proxyAdminAddress = 0x076761865E04846E76BAA5f37eBa7AfeF0a69d40;
+        address deUSDAddress = 0x15700B564Ca08D9439C58cA5053166E8317aa138;
+        address mailboxAddress = 0xc005dc82818d67AF737725bD4bf75435d065D239;
+
+        // Proxies
         ITransparentUpgradeableProxy proxy = ITransparentUpgradeableProxy(
-            payable(0x9AD81058c6C3Bf552C9014CB30E824717A0ee21b)
+            payable(proxyAddress)
+        );
+        ProxyAdmin proxyAdmin = ProxyAdmin(proxyAdminAddress);
+
+        // Deploy implementation
+        HypERC20Collateral hypERC20Collateral = new HypERC20Collateral(
+            deUSDAddress,
+            mailboxAddress
         );
 
-        ProxyAdmin proxyAdmin = ProxyAdmin(
-            0x076761865E04846E76BAA5f37eBa7AfeF0a69d40
-        );
+        // Upgrade
+        proxyAdmin.upgrade(proxy, address(hypERC20Collateral));
 
-        proxyAdmin.upgrade(proxy, 0x67f9C8460ab81b357e16D93d3799AD8e90D74B0c);
+        // Check storage
+        HypERC20Collateral upgradedProxy = HypERC20Collateral(proxyAddress);
+        require(address(upgradedProxy.wrappedToken()) == deUSDAddress);
+        require(address(upgradedProxy.mailbox()) == mailboxAddress);
+        require(upgradedProxy.owner() == deployer);
+
         vm.stopBroadcast();
     }
 
